aeb47a16af9c5807550cf91678bf03297ea4e1ae
AngelinaDerman <angelina.derman@gmail.com>
2021-03-12 13:30:18 +0300

    feat: decision reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        let str = String(n);
+        return Number(str.split("").reverse().join(""));
+    }
+    else {
+        let str = String(n).split("").reverse().join("");
+        return Number(str.substring(0, str.length - 1));
+    }
 }
